c58b67fbddd238e9919fb605aebaec9ff94552ec
Tyler Tellez <softwaretjt@gmail.com>
2023-06-23 23:36:30 -0700

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,29 @@ import './App.css';
 import Header from '../Header/Header';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import ItemCard from '../ItemCard/ItemCard';
-// import { getWeather } from '../../utils/WeatherApi';
+
+/* 
+const latitude = 34.055569;
+const longitude = -117.182541;
+const APIkey = `393864ae7bb46428ae703a86fe0dd5d7`;
+
+export const getWeather = () => {
+  const weatherApi = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+  ).then(checkResponse);
+
+  return weatherApi;
+};
+
+const checkResponse = (res) => {
+  console.log(res);
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+};
+
+ */
 
 function App() {
   /*   useEffect(() => {
